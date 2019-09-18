cc6bdee2b770015313a5155d4941544e9c6a2af9
palmipo <palmipo@free.fr>
2019-09-18 17:32:16 +0200

    clignotant capteur lumiere

--- src/ctrl/raspberry/gpio/test/out.cpp
+++ src/ctrl/raspberry/gpio/test/out.cpp
@@ -5,6 +5,8 @@
 
 #include <linux/gpio.h>
 #include <sstream>
+#include <thread>
+#include <cstdlib>
 
 class Test : public GpioFactory
 {
@@ -40,17 +42,35 @@ int main(int argc, char **argv)
 {
 	try
 	{
+		uint8_t PIN[] = { 18, 17, 27, 23, 22, 24, 10, 25, 9, 8, 11, 7, 5, 12, 6, 13 };
 		Test factory;
 
 		uint8_t valeur = 0;
-		factory.output(18)->write(&valeur, 1);
-		
+		for (int32_t i=0; i<16; ++i)
+		{
+			factory.output(PIN[i])->write(&valeur, 1);
+		}
 
-		factory.event(4, GPIOEVENT_REQUEST_BOTH_EDGES, GPIOHANDLE_REQUEST_ACTIVE_LOW);
+//		factory.event(4, GPIOEVENT_REQUEST_BOTH_EDGES);
 
 		while(1)
 		{
-			int32_t etat = factory.scrute(1000);
+//			int32_t etat = factory.scrute(1000);
+
+			valeur = random() % 7;
+			uint8_t val1 = valeur & 1;
+			uint8_t val2 = valeur & 2;
+			uint8_t val3 = valeur & 4;
+			factory.output(PIN[0])->write(&val1, 1);
+			factory.output(PIN[3])->write(&val1, 1);
+
+			factory.output(PIN[1])->write(&val2, 1);
+			factory.output(PIN[4])->write(&val2, 1);
+
+			factory.output(PIN[2])->write(&val3, 1);
+			factory.output(PIN[5])->write(&val3, 1);
+			std::this_thread::sleep_for(std::chrono::milliseconds(250));
+
 
 		}
 	}
