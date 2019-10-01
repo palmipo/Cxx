0adec4bbe46e97b0d2ce9e36ec8757495a636a59
palmipo <palmipo@free.fr>
2019-10-01 16:03:34 +0200

    fwup modbus cmd

--- src/ctrl/raspberry/rs232/src/rs232factory.cpp
+++ src/ctrl/raspberry/rs232/src/rs232factory.cpp
@@ -1,5 +1,6 @@
 #include "rs232factory.h"
 #include "rs232.h"
+#include "rs232fifo.h"
 #include "rs232exception.h"
 #include "log.h"
 #include <cerrno>
@@ -21,6 +22,24 @@ RS232 * RS232Factory::add(const std::string & device_p)
 	catch(...)
     {
 		RS232 * serial = new RS232(device_p);
+		if (serial)
+		{
+			_port[device_p] = serial->handler();
+			PollFactory::add(serial);
+			return serial;
+		}
+    }
+}
+
+RS232Fifo * RS232Factory::addFifo(const std::string & device_p)
+{
+    try
+	{
+		return (RS232Fifo *)get(device_p);
+	}
+	catch(...)
+    {
+		RS232Fifo * serial = new RS232Fifo(device_p);
 		if (serial)
 		{
 			_port[device_p] = serial->handler();
@@ -57,17 +76,17 @@ void RS232Factory::del(const std::string & device_p)
 	throw RS232Exception(__FILE__, __LINE__, ss.str());
 }
 
-int32_t RS232Factory::actionIn(PollDevice* device)
-{
-    return device->actionIn();
-}
+//~ int32_t RS232Factory::actionIn(PollDevice* device)
+//~ {
+    //~ return device->actionIn();
+//~ }
 
-int32_t RS232Factory::actionOut(PollDevice* device)
-{
-    return device->actionOut();
-}
+//~ int32_t RS232Factory::actionOut(PollDevice* device)
+//~ {
+    //~ return device->actionOut();
+//~ }
 
-int32_t RS232Factory::actionError(PollDevice* device)
-{
-    return device->actionError();
-}
+//~ int32_t RS232Factory::actionError(PollDevice* device)
+//~ {
+    //~ return device->actionError();
+//~ }
