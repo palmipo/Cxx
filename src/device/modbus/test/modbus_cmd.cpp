57cf5755ae7bd276e8cc08ba52b9775e16e659e7
palmipo <palmipo@free.fr>
2019-10-28 10:26:21 +0100

    maj prg test modbus

--- src/device/modbus/test/modbus_cmd.cpp
+++ src/device/modbus/test/modbus_cmd.cpp
@@ -6,9 +6,7 @@
 #include <cstdint>
 #include <poll.h>
 #include <thread>
-#include <log4cxx/logger.h>
-#include <log4cxx/basicconfigurator.h>
-#include <log4cxx/helpers/exception.h>
+#include <sstream>
 
 
 static void * thread_start(Modbus::ModbusFactory * factory, int32_t * fin)
@@ -19,15 +17,19 @@ static void * thread_start(Modbus::ModbusFactory * factory, int32_t * fin)
 	{
 		try
 		{
-			factory->scrute(1000);
+			factory->scrute(1000, 1, 1, 1);
 		}
 		catch(Modbus::ModbusException e)
 		{
-			//~ Log::getLogger()->debug(__FILE__, __LINE__, "thread exception " << e.what());
+			std::stringstream ss;
+			ss << "thread exception " << e.what();
+			Log::getLogger()->debug(__FILE__, __LINE__, ss.str());
 		}
 		catch(PollException e)
 		{
-			//~ Log::getLogger()->debug(__FILE__, __LINE__, "thread exception " << e.what());
+			std::stringstream ss;
+			ss << "thread exception " << e.what();
+			Log::getLogger()->debug(__FILE__, __LINE__, ss.str());
 		}
 		catch(...)
 		{
@@ -40,57 +42,59 @@ static void * thread_start(Modbus::ModbusFactory * factory, int32_t * fin)
 
 int main(int argc, char **argv)
 {
-	if (argc != 3)
+	if (argc != 2)
 	{
-		//~ Log::getLogger()->debug(__FILE__, __LINE__, std::string(argv[0]) << " <ip>");
+		std::stringstream ss;
+		ss << std::string(argv[0]) << " <ip>";
+		Log::getLogger()->debug(__FILE__, __LINE__, ss.str());
 	}
+
+	int32_t fin = 0;
+	Modbus::ModbusFactory factory;
+
+	std::thread t1(thread_start, &factory, &fin);
+	t1.detach();
 
 	try
 	{
-		int32_t fin = 0;
-
-		Modbus::ModbusFactory factory;
-		// Modbus::ModbusChannel * sock = (Modbus::ModbusChannel *)factory.tcp(argv[1]);
-		Modbus::ModbusChannel * sock1 = (Modbus::ModbusChannel *)factory.rtu(argv[1], 19200, 1, 1);
-
-		std::thread t1(thread_start, &factory, &fin);
-		t1.detach();
+		Modbus::ModbusChannel * sock1 = (Modbus::ModbusChannel *)factory.tcp(argv[1]);
+		//~ Modbus::ModbusChannel * sock1 = (Modbus::ModbusChannel *)factory.rtu(argv[1], 19200, 1, 1);
 
 		Modbus::ModbusMsgFC03 msg1;
 		msg1.setSlaveAddress(0xF8);
 		msg1.setRegisterAddr(0xCA8, 1);
-
-		uint16_t len = 0;
-		len = sock1->sendFC(&msg1);
-
-		len = sock1->recvFC(&msg1);
-		if (len)
-		{
-			//~ Log::getLogger()->debug(__FILE__, __LINE__, "sock->recvFC() : " << len);
-			//~ Log::getLogger()->debug(__FILE__, __LINE__, "slave addr : " << (int)msg1.slaveAddress());
-			//~ Log::getLogger()->debug(__FILE__, __LINE__, "fonction code : " << (int)msg1.functionCode());
-			//~ Log::getLogger()->debug(__FILE__, __LINE__, "error code : " << (int)msg1.errorCode());
-			//~ Log::getLogger()->debug(__FILE__, __LINE__, "TCC : " << (int)msg1.getRegister(11101));
-		}
-
-		std::this_thread::sleep_for(std::chrono::seconds(60));
-
-		// arret du thread secondaire
-		fin = 1;
-		t1.join();
+		msg1.encodeQuestion();
+		sock1->sendFC(&msg1);
+		msg1.decodeResponse();
+		std::stringstream ss;
+		ss << "slave addr : " << (int)msg1.slaveAddress();
+		ss << " fonction code : " << (int)msg1.functionCode();
+		ss << " error code : " << (int)msg1.errorCode();
+		ss << " TCC : " << (int)msg1.getRegister(0xCA8);
+		Log::getLogger()->debug(__FILE__, __LINE__, ss.str());
 	}
 	catch(Modbus::ModbusException e)
 	{
-	//~ Log::getLogger()->debug(__FILE__, __LINE__, "ModbusException : " << e.what());
+		std::stringstream ss;
+		ss << "ModbusException : " << e.what();
+		Log::getLogger()->debug(__FILE__, __LINE__, ss.str());
 	}
 	catch(PollException e)
 	{
-	//~ Log::getLogger()->debug(__FILE__, __LINE__, "PollException : " << e.what());
+		std::stringstream ss;
+		ss << "PollException : " << e.what();
+		Log::getLogger()->debug(__FILE__, __LINE__, ss.str());
 	}
 	catch(...)
 	{
-	Log::getLogger()->debug(__FILE__, __LINE__, "exception ...");
+		Log::getLogger()->debug(__FILE__, __LINE__, "exception ...");
 	}
+
+	// arret du thread secondaire
+	fin = 1;
+
+	std::this_thread::sleep_for(std::chrono::seconds(60));
+	//~ t1.join();
 
 	return 0;
 }
