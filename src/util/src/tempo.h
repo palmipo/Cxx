d66291ccb89dc666d31c7fe095415b7368c5ff9d
palmipo <55411002+palmipo@users.noreply.github.com>
2022-02-22 11:20:16 +0100

    Create tempo.h
    Classe virtuelle pure de temporisation

---
+++ src/util/src/tempo.h
@@ -0,0 +1,13 @@
+#ifndef TEMPO_H
+#define TEMPO_H
+
+Class Tempo
+{
+Public:
+static void minute(uint32_t) = 0;
+static void seconde(uint32_t) = 0;
+static void milliseconde(uint32_t) = 0;
+static void microseconde(uint32_t) = 0;
+};
+
+#endif
