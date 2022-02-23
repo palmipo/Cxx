dc21ca4dcf6b23462f0c7d02821f3f5d4d463964
palmipo <palmipo@free.fr>
2022-02-23 18:51:19 +0100

    commande pour git

--- git_commit.sh
+++ git_commit.sh
@@ -1,5 +1,6 @@
-find . -type f  -name *.h -exec git add {}   \;
-find . -type f  -name *.cpp -exec git add {}   \;
-find . -type f  -name Makefile -exec git add {}   \;
-git commit -m $1
+find . -type f  -name *.h -exec git add {} \;
+find . -type f  -name *.cpp -exec git add {} \;
+find . -type f  -name *.pdf -exec git add {} \;
+find . -type f  -name Makefile -exec git add {} \;
+git commit -m $@
 git push
