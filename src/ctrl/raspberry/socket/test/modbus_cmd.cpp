c55004fb62501c0ccb82573fa0955bd893c3d395
palmipo <55411002+palmipo@users.noreply.github.com>
2022-03-01 18:27:27 +0100

    Update modbus_cmd.cpp

--- src/ctrl/raspberry/socket/test/modbus_cmd.cpp
+++ src/ctrl/raspberry/socket/test/modbus_cmd.cpp
@@ -4,22 +4,6 @@
 #include "log.h"
 #include <sstream>
 
-class Factory : public Socket::SocketFactory
-{
-	public:
-		Factory()
-		{}
-
-		virtual int32_t actionIn(PollDevice * device)
-		{}
-
-		virtual int32_t actionOut(PollDevice * device)
-		{}
-
-		virtual int32_t actionError(PollDevice * device)
-		{}
-};
-
 int main(int argc, char **argv)
 {
 	if (argc != 2)
@@ -32,16 +16,15 @@ int main(int argc, char **argv)
 
 	try
 	{
-		Factory factory;
+		Socket::SocketFactory factory;
 		Socket::SocketTcp * sock = (Socket::SocketTcp *)factory.addTcpConnection(argv[1], 502);
 		if (sock)
 		{
 			uint8_t msg[] = {0x00, 0x01, 0x00, 0x00, 0x00, 0x06, 0xF8, 0x10, 0x00, 0x01, 0x02, 0x00, 0xFF};
+			sock->write(msg, 13);
 
-			int32_t len = sock->write(msg, 13);
-			factory.scrute(10000);
 			uint8_t rep[100];
-			len = sock->read(rep, 100);
+			sock->read(rep, 100);
 		}
 	}
 	catch(Socket::SocketException e)
