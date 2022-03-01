7122e9a5c8d44122d5c10b2a86a7616efa554636
palmipo <55411002+palmipo@users.noreply.github.com>
2022-03-01 10:01:51 +0100

    Update polldevice.h

--- src/ctrl/raspberry/poll/src/polldevice.h
+++ src/ctrl/raspberry/poll/src/polldevice.h
@@ -7,10 +7,9 @@
 class PollDevice : public Device
 {
 	public:
-enum { GPIO, UART, SPI, I2C, JOYSTICK } DeviceType;
 
-		PollDevice(DeviceType);
-		PollDevice(DeviceType, int32_t);
+		PollDevice(Device::Type);
+		PollDevice(Device::Type, int32_t);
 
 		virtual int32_t write(uint8_t *, int32_t) = 0;
 		virtual int32_t read(uint8_t *, int32_t) = 0;
