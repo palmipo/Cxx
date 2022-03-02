10d4efc80b2e595497d7d821048edc85e5f1c086
palmipo <palmipo@free.fr>
2022-03-02 12:58:06 +0100

    i2c

--- src/device/i2c/test/test_phat_enviro.cpp
+++ src/device/i2c/test/test_phat_enviro.cpp
@@ -1,8 +1,10 @@
 #include "phat_enviro.h"
 #include "raspigpiofactory.h"
 #include "raspigpio.h"
+#include "raspipia.h"
 #include "tcs34725.h"
 #include "raspii2c.h"
+#include "tempo.h"
 #include "log.h"
 #include <thread>
 #include <sstream>
@@ -10,25 +12,28 @@
 int main(int argc, char **argv)
 {
 	RaspiGpioFactory factory("/dev/gpiochip0");
-	RaspiGpio *io = factory.output(4);
+	int32_t pin = 4;
+	RaspiGpio *io = factory.outputs(&pin, 1);
+	RaspiPia pia(io);
 
-	std::this_thread::sleep_for(std::chrono::seconds(5));
+	Tempo::secondes(5);
 
-	PHatEnviro pHat(io);
+	PHatEnviro pHat(&pia);
 
-	std::this_thread::sleep_for(std::chrono::seconds(5));
+	Tempo::secondes(5);
 
 	pHat.setLed(1);
 
-	std::this_thread::sleep_for(std::chrono::seconds(5));
+	Tempo::secondes(5);
 
 	RaspiI2C i2c("/dev/i2c-1");
 	TCS34725 capteur_couleur(&i2c);
+
 	std::stringstream ss;
 	ss << (uint32_t)capteur_couleur.id();
 	Log::getLogger()->debug(__FILE__, __LINE__, ss.str());
 	
-	//~ capteur_couleur.on(0, 0, 1, 1);
+	pHat.setLed(1);
 	
 	return 0;
 }
