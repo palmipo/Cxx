d96d7756bca844040027512304a26ef7bc3c6cd7
palmipo <55411002+palmipo@users.noreply.github.com>
2022-03-07 08:57:32 +0100

    Update i2c.h

--- src/ctrl/interface/src/i2c.h
+++ src/ctrl/interface/src/i2c.h
@@ -9,10 +9,6 @@ public:
 	I2C()
 	{}
 
-	// virtual void setOwnAddress (uint8_t own_address) = 0;
-	// virtual void read (uint8_t* buf, int32_t len) = 0;
-	// virtual void write (uint8_t* buf, int32_t len) = 0;
-
 	virtual void set (uint8_t addr, uint8_t* buf, int32_t len) = 0;
 	virtual void get (uint8_t addr, uint8_t* buf, int32_t len) = 0;
 	virtual void transfert (uint8_t addr, uint8_t* cmd, int32_t cmd_len, uint8_t* buf, int32_t buf_len) = 0;
