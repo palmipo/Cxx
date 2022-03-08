42365c81b676f8d4ec554d4a429a2d6ebaf4823a
palmipo <55411002+palmipo@users.noreply.github.com>
2022-03-08 14:49:50 +0100

    Update polldevice.cpp

--- src/ctrl/raspberry/poll/src/polldevice.cpp
+++ src/ctrl/raspberry/poll/src/polldevice.cpp
@@ -2,8 +2,10 @@
 
 PollDevice::PollDevice(enum Device::DeviceType type)
 : Device(type)
+, _handler(-1)
 {}
 
 PollDevice::PollDevice(enum Device::DeviceType type, int32_t handler)
-: Device(type, handler)
+: Device(type)
+, _handler(handler)
 {}
