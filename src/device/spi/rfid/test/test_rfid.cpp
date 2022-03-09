adcc80ac4029548049b8123d9bd931e928d9b168
palmipo <55411002+palmipo@users.noreply.github.com>
2022-03-09 13:11:02 +0100

    Update test_rfid.cpp

--- src/device/spi/rfid/test/test_rfid.cpp
+++ src/device/spi/rfid/test/test_rfid.cpp
@@ -1,13 +1,22 @@
 #include "raspispi.h"
 #include "spiexception.h"
 #include "mfrc522.h"
+#include <cstdint>
+#include <iostream>
+#include <sstream>
 
-int main(char **argv, int argc)
+int main(int argc, char **argv)
 {
 try
 {
 RaspiSpi spi(argv[1]);
 MFRC522 rfid(&spi);
+uint8_t a, b;
+rfid.version(&a, &b);
+
+std::stringstream ss;
+ss << "version : " << (int)a << ", " << (int)b;
+std::cout << ss.str() << std::endl;
 }
 catch (SPIExecption)
 {
