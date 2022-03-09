eb99f3ae1944af9d74e5d492506c96f391459eec
palmipo <55411002+palmipo@users.noreply.github.com>
2022-03-09 12:03:43 +0100

    Update device.h

--- src/util/src/device.h
+++ src/util/src/device.h
@@ -8,11 +8,19 @@ class Device
 	public:
 		enum DeviceType { GPIO = 1, I2C, SPI, UART, SOCKET, JOYSTICK, CAN } DeviceType;
 
-		Device(enum DeviceType);
+		Device(enum DeviceType type)
+		: _type(type)
+		{}
 
-		enum DeviceType type();
+		enum DeviceType type()
+		{
+		    return _type;
+		}
 
-		bool operator==(const Device &) const;
+		bool operator==(const Device & device) const
+		{
+			return (device._type == _type);
+		}
 
 	protected:
 		enum DeviceType _type;
