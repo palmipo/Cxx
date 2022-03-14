bcc9b09e46d1d33e518b7fb018223c55a901e4b6
palmipo <55411002+palmipo@users.noreply.github.com>
2022-03-14 10:11:14 +0100

    Update pcf8574a.cpp

--- src/device/i2c/src/pcf8574a.cpp
+++ src/device/i2c/src/pcf8574a.cpp
@@ -8,24 +8,24 @@ PCF8574A::PCF8574A(uint8_t address, CtrlI2C * i2c)
 
 // 1 : INPUT
 // 0 : OUTPUT
-void PCF8574A::setDirection(uint8_t port, uint8_t direction)
+void PCF8574A::setDirection(uint8_t direction)
 {
 	_direction = direction;
 	_twi->set(_address, &_direction, 1);
 }
 
-uint8_t PCF8574A::getDirection(uint8_t port)
+uint8_t PCF8574A::getDirection()
 {
 	return _direction;
 }
 
-void PCF8574A::set (uint8_t port, uint8_t valeur)
+void PCF8574A::set (uint8_t valeur)
 {
 	uint8_t byte = valeur | _direction;
 	_twi->set(_address, &byte, 1);
 }
 
-uint8_t PCF8574A::get (uint8_t port)
+uint8_t PCF8574A::get ()
 {
 	uint8_t valeur = 0;
 	_twi->get(_address, &valeur, 1);
