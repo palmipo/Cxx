19b22dc3703ef4072b9a82c11a1c9a010d282d3b
palmipo <55411002+palmipo@users.noreply.github.com>
2022-03-17 11:11:15 +0100

    Update rs485.h

--- src/device/rs232/rs485/src/rs485.h
+++ src/device/rs232/rs485/src/rs485.h
@@ -1,17 +1,23 @@
 #ifndef RS485_H
 #define RS485_H
 
-#include "uart.h"
 #include <cstdint>
 
-class RS232;
-class RS485 : public Uart
+class PIA;
+class Uart;
+class RS485
 {
-RS485(RS232 *, PIA * = 0, PIA * = 0);
+public:
+RS485(Uart *, PIA * = 0, PIA * = 0);
 virtual ~RS485();
 
 virtual int32_t read(uint8_t, int32_t);
 virtual int32_t write(uint8_t, int32_t);
+
+private:
+Uart * _uart;
+PIA * _write_pin;
+PIA * _read_pin;
 };
 
 #endif /* RS485_H */
