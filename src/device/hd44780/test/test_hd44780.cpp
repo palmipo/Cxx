431db48cd27c046336a9a2c31f737a55a0b97e1b
palmipo <palmipo@free.fr>
2022-03-20 21:35:25 +0100

    hd44780

--- src/device/hd44780/test/test_hd44780.cpp
+++ src/device/hd44780/test/test_hd44780.cpp
@@ -29,8 +29,34 @@ static int32_t irq(PollDevice * gpio, void * user_data)
 	std::cout << "irq leve !!!!" << std::endl;
 	try
 	{
+		uint32_t id;
+		uint64_t heure;
+		((RaspiGpio *)gpio)->readEvent(&id, &heure);
+		std::cout << "reception de " << id << " timestamp:" << heure << std::endl;
+
+		/*
 		ADA772 * lcd_io = (ADA772 *)user_data;
 		lcd_io->scrute();
+		*/
+		/*
+		HD44780 * lcd = (HD44780 *)user_data;
+
+		std::string str_date;
+		struct tm *la_date;
+		long t;
+		time(&t);
+		la_date = localtime(&t);
+		std::stringstream buf_date;
+		buf_date << std::setw(2) << std::setfill('0') << la_date->tm_mday << "-" << std::setw(2) << std::setfill('0') << 1 + la_date->tm_mon << "-" << std::setw(4) << std::setfill('0') << 1900 + la_date->tm_year << " " << std::setw(2) << std::setfill('0') << la_date->tm_hour <<":" << std::setw(2) << std::setfill('0') << la_date->tm_min;
+
+		str_date = buf_date.str();
+
+		lcd->setPosition(0, 0);
+		lcd->setText((int8_t *)str_date.c_str(), str_date.length());
+
+		lcd->setPosition(1, 0);
+		lcd->setText((int8_t *)"20 C", 4);
+		*/
 	}
 	catch(...)
 	{
@@ -57,7 +83,6 @@ int main(int argc, char **argv)
 		RaspiI2C i2c("/dev/i2c-1");
 		MCP23017 pia(0, &i2c);
 
-		// virtual void init(u8 port, u8 dir=0, u8 pol=0, u8 pullup=0, u8 irq=0, u8 defval_pin=0, u8 defval=0);
 		pia.init(0, 0x1F, 0x1F, 0x1F, 0x1F);
 		pia.init(1, 0, 0, 0);
 
@@ -65,43 +90,22 @@ int main(int argc, char **argv)
 		lcd_io.setBackLight(1);
 		lcd_io.setButtonsCallback(bouton, 0);
 
-		HD44780 lcd(&lcd_io, 16, 2);
+		HD44780 lcd(&lcd_io, 2, 16);
 
 		RaspiGpioFactory gpio_fact("/dev/gpiochip0");
-		RaspiGpio * gpio = gpio_fact.event(4);
+		RaspiGpio * gpio = gpio_fact.event(4, GPIOEVENT_REQUEST_RISING_EDGE);
 
 		PollFactory poll_fact;
-		poll_fact.setActionInCallback(irq, &lcd_io);
+		poll_fact.setActionInCallback(irq, &lcd);
+		//poll_fact.setActionInCallback(irq, &lcd_io);
 		poll_fact.add(gpio);
 
 		int32_t fin = 0;
 
 		std::thread my_thread(scrute, &poll_fact, &fin);
+		my_thread.detach();
 
-		std::string str_date;
-		struct tm *la_date;
-		long t;
-		int cpt = 0;
-		while (cpt < 500)
-		{
-			time(&t);
-			la_date = localtime(&t);
-			std::stringstream buf_date;
-			buf_date << std::setw(2) << std::setfill('0') << la_date->tm_mday << "-" << std::setw(2) << std::setfill('0') << 1 + la_date->tm_mon << "-" << std::setw(4) << std::setfill('0') << 1900 + la_date->tm_year << " " << std::setw(2) << std::setfill('0') << la_date->tm_hour <<":" << std::setw(2) << std::setfill('0') << la_date->tm_min;
-			str_date = buf_date.str();
-
-			lcd.setPosition(0, 0);
-			lcd.setText((int8_t *)str_date.c_str(), str_date.length());
-
-			lcd.setPosition(1, 0);
-			lcd.setText((int8_t *)"20 C", 4);
-
-
-			std::cout << "compteur : " << cpt << std::endl;
-			++cpt;
-
-			Tempo::secondes(30);
-		}
+		Tempo::minutes(10);
 
 		fin = 1;
 		my_thread.join();
