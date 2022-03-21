99f65d109a59f1b2288df5f1fb2eef02dc549eaa
palmipo <55411002+palmipo@users.noreply.github.com>
2022-03-21 10:04:22 +0100

    Update raspipia.cpp

--- src/device/pia/src/raspipia.cpp
+++ src/device/pia/src/raspipia.cpp
@@ -14,9 +14,9 @@ void RaspiPia::write(uint8_t val)
 	uint8_t data[_gpio->pinNumber()];
 	for (int32_t i=0; i<_gpio->pinNumber(); ++i)
 	{
-		data[i] = val & (1<<i);
+		data[i] = (val & (1<<i)) ? 1 : 0;
 	}
-	_gpio->write(data, i);
+	_gpio->write(data, _gpio->pinNumber());
 }
 
 uint8_t RaspiPia::read()
