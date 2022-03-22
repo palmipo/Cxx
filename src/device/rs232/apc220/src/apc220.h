778c63d5de5a2d5000d123de8bd813f42d70613c
palmipo <55411002+palmipo@users.noreply.github.com>
2022-03-22 10:05:39 +0100

    Update apc220.h

--- src/device/rs232/apc220/src/apc220.h
+++ src/device/rs232/apc220/src/apc220.h
@@ -8,17 +8,18 @@ class RS232;
 class APC220
 {
 	public:
-		APC220(PIA *, PIA *, RS232 *);
+		APC220(PIA *, PIA *, PIA *, RS232 *);
 		virtual ~APC220();
 	
 		void setConfig(int32_t, int32_t, int32_t, int32_t, int32_t);
 		void getConfig();
-		int32_t get(uint8_t*, int32_t);
-		int32_t set(uint8_t*, int32_t);
+		int32_t read(uint8_t*, int32_t);
+		int32_t write(uint8_t*, int32_t);
 
 	protected:
 		PIA * _pin_set;
 		PIA * _pin_enable;
+PIA * _pin_aux;
 		RS232 * _uart;
 };
 
