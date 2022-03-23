9ed072e2b2f2f3055111219913540b7cbfc83aaa
palmipo <55411002+palmipo@users.noreply.github.com>
2022-03-23 10:17:25 +0100

    Update apc220.cpp

--- src/device/rs232/apc220/src/apc220.cpp
+++ src/device/rs232/apc220/src/apc220.cpp
@@ -5,6 +5,7 @@
 #include "apc220.h"
 #include "pia.h"
 #include "rs232.h"
+#include "tempo.h"
 #include <iostream>
 #include <sstream>
 
@@ -18,6 +19,7 @@ APC220::APC220(PIA * pin, RS232 * uart)
 	_uart->setConfig(B9600, 8, 'N', 1, 0);
 	_uart->setInterCharacterTimer(0xFF);
 	_pin->write((1<<SET)|(1<<EN)|(0<<AUX));
+	Tempo::millisecondes(50);
 }
 
 APC220::~APC220()
@@ -28,6 +30,7 @@ APC220::~APC220()
 void APC220::setConfig(int32_t frequency, int32_t air_data_rate, int32_t output_power, int32_t data_rate_in, int32_t parity)
 {
 	_pin->write((0<<SET)|(1<<EN)|(0<<AUX));
+	Tempo::millisecondes(10);
 
 	std::stringstream ss;
 	ss << "WR" << (uint8_t)0x20 << "434000" << (uint8_t)0x20 << "3" << (uint8_t)0x20 << "9" << (uint8_t)0x20 << "0" << (uint8_t)0x20 << "0" << (uint8_t)0x0D << (uint8_t)0x0A;
@@ -39,11 +42,13 @@ void APC220::setConfig(int32_t frequency, int32_t air_data_rate, int32_t output_
 	std::cout << __FILE__ << __LINE__ << msg << std::endl;
 
 	_pin->write((1<<SET)|(1<<EN)|(0<<AUX));
+	Tempo::millisecondes(50);
 }
 
 void APC220::getConfig()
 {
 	_pin->write((0<<SET)|(1<<EN)|(0<<AUX));
+	Tempo::millisecondes(10);
 
 	std::stringstream ss;
 	ss << "RD" << (uint8_t)0x0D << (uint8_t)0x0A;
@@ -55,15 +60,18 @@ void APC220::getConfig()
 	std::cout << __FILE__ << __LINE__ << msg << std::endl;
 
 	_pin->write((1<<SET)|(1<<EN)|(0<<AUX));
+	Tempo::millisecondes(50);
 }
 
 int32_t APC220::read(uint8_t *msg, int32_t len)
 {
 	_pin->write((1<<SET)|(1<<EN)|(0<<AUX));
+	Tempo::millisecondes(50);
 
 	int32_t length = _uart->read(msg, len);
 
 	_pin->write((1<<SET)|(1<<EN)|(0<<AUX));
+	Tempo::millisecondes(50);
 
 	return length;
 }
@@ -71,10 +79,12 @@ int32_t APC220::read(uint8_t *msg, int32_t len)
 int32_t APC220::write(uint8_t *msg, int32_t len)
 {
 	_pin->write((1<<SET)|(1<<EN)|(1<<AUX));
+	Tempo::millisecondes(50);
 
 	int32_t length = _uart->write(msg, len);
 
 	_pin->write((1<<SET)|(1<<EN)|(0<<AUX));
+	Tempo::millisecondes(50);
 
 	return length;
 }
