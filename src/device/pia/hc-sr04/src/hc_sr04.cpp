d8f18a0c6b3deb151f57ddb361bb72d684a03b61
palmipo <55411002+palmipo@users.noreply.github.com>
2022-03-28 15:15:58 +0200

    Update hc_sr04.cpp

--- src/device/pia/hc-sr04/src/hc_sr04.cpp
+++ src/device/pia/hc-sr04/src/hc_sr04.cpp
@@ -1,11 +1,8 @@
 #include "hc_sr04.h"
-#include "pia.h"
+#include "ctrlpia.h"
 #include "tempo.h"
-#include "log.h"
-#include <sstream>
-#include <chrono>
 
-HC_SR04::HC_SR04(PIA * OUT_PIN)
+HC_SR04::HC_SR04(CtrlPia * OUT_PIN)
 : _distance(0)
 , _status(0)
 , _out(OUT_PIN)
