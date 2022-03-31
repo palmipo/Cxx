677324676665f80b1e1599a9fe9c306164578c65
palmipo <55411002+palmipo@users.noreply.github.com>
2022-03-31 09:05:30 +0200

    Update test_apc220.cpp

--- src/device/rs232/apc220/test/test_apc220.cpp
+++ src/device/rs232/apc220/test/test_apc220.cpp
@@ -2,12 +2,14 @@
 #include "rs232factory.h"
 #include "rs232.h"
 #include "raspigpiofactory.h"
+#include "raspigpio.h"
 #include "raspipia.h"
+#include "raspiuart.h"
 
 int main(int argc, char **argv)
 {
 	RS232Factory fact;
-	RS232 * uart = fact.add("/dev/ttyAMA0");
+	RS232 * rs232 = fact.add("/dev/ttyAMA0");
 
 	// SET, EN, AUX
 	int32_t pin[] = { 10, 11, 12 };
@@ -15,9 +17,10 @@ int main(int argc, char **argv)
 	RaspiGpioFactory gpio("/dev/gpiochip0");
 	RaspiGpio * out = gpio.outputs(pin, 3);
 
+RaspiUart uart(rs232);
 	RaspiPia pia(out);
 
-	APC220 radio(&pia, uart);
+	APC220 radio(&pia, &uart);
 	radio.setConfig(434000, 3, 9, 0, 0);
 	radio.getConfig();
 
