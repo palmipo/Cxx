a81d5f0139ef5976e593bb51666d9b968151ebbc
palmipo <55411002+palmipo@users.noreply.github.com>
2022-10-18 08:46:50 +0200

    Update codec.h

--- src/util/src/codec.h
+++ src/util/src/codec.h
@@ -9,7 +9,7 @@ class Codec
 		Codec();
 		virtual ~Codec();
 	
-		void encoder_decoder(uint8_t*, int32_t, uint8_t*, int32_t, int32_t, int32_t=0);
+		void encoder_decoder(uint8_t*, int32_t, uint8_t*, int32_t, int32_t, int32_t=0, int32_t=0);
 };
 
 #endif /* CODEC_H */
